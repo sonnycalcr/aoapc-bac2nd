d6dfa125c392651368451c2848e9bafff11359e8
fanlumaster <1730976608@qq.com>
2024-09-05 07:08:15 +0800

    init project

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "main.h"
 
 int main(int argc, char *argv[]) {
-    std::cout << "This is a cpp template for Linux.\n";
+    std::cout << "Welcome to my UVa repo for aoapc-bac2nd!\n";
     return 0;
 }
